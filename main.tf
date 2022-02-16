317b8e7241be4463f2decd208c529a5f399b8c18
vkazoo <kazu.toku@gmail.com>
2022-02-16 07:01:22 +0000

    forth commit.

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "dev1"
+    Department = "devops"
     Billable = "true"
   }
 }
